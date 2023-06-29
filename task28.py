d8377cd32e8bde5b06246c7c31ee21b0358e440c
Tulaev Muhammad <izzatullohman@gmail.com>
2023-06-29 22:00:12 +0500

    Created task28.py
    28th problem on leetcode solved

---
+++ task28.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 21:45:10 2023
+
+@author: muhammad
+"""
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
