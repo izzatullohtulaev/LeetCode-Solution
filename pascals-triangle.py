cbeb1e41b8213b1a599d491f3205991b9ccfd8d1
Izzatulloh Tulayev <106700130+izzatullohtulaev@users.noreply.github.com>
2023-07-24 01:54:36 +0500

    asd

--- pascals-triangle.py
+++ pascals-triangle.py
@@ -1,9 +1,25 @@
 def generate(numRows: int):
-    superarray = [[1]]
+    superarray = []
     subarray = [1]
-    superarray += subarray
+    old_subarray = [1]
     if numRows == 1:
-        return superarray
+        print(subarray)
+    else:
+        for row in range(1, numRows+1):
+            subarray = []
+            for i in range(row):
+                if i == 0:
+                    subarray.append(old_subarray[0]+0)
+                elif i == row-1:
+                    subarray.append(old_subarray[-1]+0)
+                else:
+                    subarray.append(old_subarray[i]+old_subarray[i-1])
+            # print(subarray)
+            superarray.append(subarray)
+            old_subarray = subarray
+    return superarray
 
 
-print(generate(1))
+print(generate(5))
+
+# rows = int(input("The number of the rows: "))
